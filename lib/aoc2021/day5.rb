4733ed8db987239cb29a89153d45599c796c9b49
caneylan <caneylan>
2021-12-05 01:21:42 -0500

    2021 day 5

---
+++ lib/aoc2021/day5.rb
@@ -0,0 +1,39 @@
+
+module Aoc2021
+  class Day5 < Solution
+
+    def map_vents(skip_diagonals = false)
+      vent_lines = Hash.new { |h, k| h[k] = Hash.new(0) }
+      input.each do |line|
+        a, _, b = line.split
+        a_x, a_y = a.split(',').map(&:to_i)
+        b_x, b_y = b.split(',').map(&:to_i)
+
+        x_step = a_x < b_x ? 1 : a_x == b_x ? 0 : -1
+        y_step = a_y < b_y ? 1 : a_y == b_y ? 0 : -1
+
+        next unless a_x == b_x || a_y == b_y if skip_diagonals
+
+        begin
+          vent_lines[a_x][a_y] += 1
+          a_x += x_step
+          a_y += y_step
+        end until a_x == b_x && a_y == b_y
+        vent_lines[a_x][a_y] += 1
+      end
+
+      return vent_lines.map { |x, col| col.count { |y, v| v > 1 } }.sum
+    end
+
+    def part1!
+      return map_vents(true)
+    end
+
+    def part2!
+      return map_vents
+    end
+
+  end
+end
+
+
---
+++ spec/aoc2021-day5_spec.rb
@@ -0,0 +1,27 @@
+
+require 'aoc2021'
+
+RSpec.describe Aoc2021::Day5 do
+
+  it "solves part 1 example" do
+    day = Aoc2021::Day5.new ::File.expand_path("../input/2021/day5/input1-example", __dir__)
+    expect(day.part1!).to eq(5)
+  end
+
+  it "solves part 1" do
+    day = Aoc2021::Day5.new ::File.expand_path("../input/2021/day5/input1", __dir__)
+    expect(day.part1!).to eq(4826)
+  end
+
+  it "solves part 2 example" do
+    day = Aoc2021::Day5.new ::File.expand_path("../input/2021/day5/input1-example", __dir__)
+    expect(day.part2!).to eq(12)
+  end
+
+  it "solves part 2" do
+    day = Aoc2021::Day5.new ::File.expand_path("../input/2021/day5/input1", __dir__)
+    expect(day.part2!).to eq(16793)
+  end
+
+end
+
