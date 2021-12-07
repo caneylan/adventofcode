74e4f1eeac38fb8b47ea6f4a03e7b37aa10dd94d
caneylan <caneylan>
2021-12-07 00:37:38 -0500

    2021 day 7
    showerthought: the fuel costs form a well, where once you find the
    bottom of the well, you can stop searching because all of the costs
    after that will continue to increase.
    
    this also means that searching linearly like this is sub-optimal on
    a large data set and it'd be faster to binary-search the space.

--- lib/aoc2021/day7.rb
+++ lib/aoc2021/day7.rb
@@ -3,11 +3,12 @@ module Aoc2021
 
     def part1!
       positions = input.first.split(',').map(&:to_i).sort
-      min_fuel_cost = positions.last ** 2
+      min_fuel_cost = nil
 
       positions.first.upto(positions.last) do |position|
         fuel_cost = positions.inject(0) { |sum, p| sum += (position - p).abs }
-        min_fuel_cost = fuel_cost if fuel_cost < min_fuel_cost
+        min_fuel_cost = fuel_cost if min_fuel_cost.nil? or fuel_cost < min_fuel_cost
+        break if fuel_cost > min_fuel_cost
       end
 
       return min_fuel_cost
@@ -15,11 +16,12 @@ module Aoc2021
 
     def part2!
       positions = input.first.split(',').map(&:to_i).sort
-      min_fuel_cost = positions.last ** 3
+      min_fuel_cost = nil
 
       positions.first.upto(positions.last) do |position|
         fuel_cost = positions.inject(0) { |sum, p| d = (position - p).abs ;  sum += d * (d + 1) / 2 }
-        min_fuel_cost = fuel_cost if fuel_cost < min_fuel_cost
+        min_fuel_cost = fuel_cost if min_fuel_cost.nil? or fuel_cost < min_fuel_cost
+        break if fuel_cost > min_fuel_cost
       end
 
       return min_fuel_cost
