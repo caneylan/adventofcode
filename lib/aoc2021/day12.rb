f987c351af194bcc9fb6a99d49cfd849481d3d85
caneylan <caneylan>
2021-12-12 15:03:10 -0500

    2021 day 12

--- lib/aoc2021/day12.rb
+++ lib/aoc2021/day12.rb
@@ -16,9 +16,9 @@ module Aoc2021
 
     # ~0.8s for all tests
     # depth-first iterative; accumulates new data-structures
-    def cave_search_iter(start_path, allow_dupe = false)
+    def cave_search_iter(allow_dupe = false)
       complete_paths = 0
-      incomplete_paths = [ start_path ]
+      incomplete_paths = [ { :last => :start } ]
       loop do
         break if incomplete_paths.empty?
         path = incomplete_paths.pop
@@ -41,38 +41,34 @@ module Aoc2021
       return complete_paths
     end
 
-    # ~0.35s for all tests
+    # ~0.32s for all tests
     # depth-first recursive; one re-used data-structure
-    def cave_search_recur(path, allow_dupe = false)
+    def cave_search_recur(from, allow_dupe = false, seen = {})
       complete_paths = 0
-      @caves[path[:last]].each do |to|
+      @caves[from].each do |to|
         if to == :end
           complete_paths += 1
-        elsif @upcase_lookup[to] || path[to].nil?
-          old_last = path[:last]
-          path[:last] = to
-          path[to] = true
-          complete_paths += cave_search_recur(path, allow_dupe)
-          path[:last] = old_last
-          path[to] = nil
-        elsif allow_dupe && path[:dupe].nil?
-          old_last = path[:last]
-          path[:last] = to
-          path[:dupe] = to
-          complete_paths += cave_search_recur(path, allow_dupe)
-          path[:last] = old_last
-          path[:dupe] = nil
+        elsif @upcase_lookup[to] || seen[to].nil?
+          seen[to] = true
+          complete_paths += cave_search_recur(to, allow_dupe, seen)
+          seen[to] = nil
+        elsif allow_dupe && seen[:dupe].nil?
+          seen[:dupe] = to
+          complete_paths += cave_search_recur(to, allow_dupe, seen)
+          seen[:dupe] = nil
         end
       end
       return complete_paths
     end
 
     def part1!
-      cave_search_recur({ :last => :start })
+      #cave_search_iter
+      cave_search_recur(:start)
     end
 
     def part2!
-      cave_search_recur({ :last => :start }, true)
+      #cave_search_iter(true)
+      cave_search_recur(:start, true)
     end
   end
 
