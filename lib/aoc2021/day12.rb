9d93e447dfd427140b40a54900a13ec79857f619
caneylan <caneylan>
2021-12-12 10:15:28 -0500

    2021 day 12

--- lib/aoc2021/day12.rb
+++ lib/aoc2021/day12.rb
@@ -4,53 +4,35 @@ module Aoc2021
     def initialize(filename)
       super(filename)
       @caves = Hash.new { |h, k| h[k] = Array.new }
+      @upcase_lookup = {}
       input.each do |line|
         a, b = line.split('-')
-        @caves[a] << b unless b == 'start'
-        @caves[b] << a unless a == 'start'
+        @caves[a.to_sym] << b.to_sym unless b == 'start'
+        @caves[b.to_sym] << a.to_sym unless a == 'start'
+        @upcase_lookup[a.to_sym] = true if a == a.upcase
+        @upcase_lookup[b.to_sym] = true if b == b.upcase
       end
     end
 
-    # terrible breadth-first accumulator
-    def part1!
+    # ~0.8s for all tests
+    # breadth-first iterative; accumulates new data-structures
+    def cave_search_bf(start_path, allow_dupe = false)
       complete_paths = 0
-      incomplete_paths = [ { :last => 'start' } ]
+      incomplete_paths = [ start_path ]
       loop do
         break if incomplete_paths.empty?
         path = incomplete_paths.pop
         @caves[path[:last]].each do |to|
-          if to == 'end'
+          if to == :end
             complete_paths += 1
-          elsif to == to.upcase || ! path[to]
+          elsif @upcase_lookup[to] || ! path[to]
             new_path = path.clone
             new_path[:last] = to
             new_path[to] = true
             incomplete_paths << new_path
-          end
-        end
-      end
-      return complete_paths
-    end
-
-    # terrible breadth-first accumulator
-    def part2!
-      complete_paths = 0
-      incomplete_paths = [ { :last => 'start' } ]
-      loop do
-        break if incomplete_paths.empty?
-        path = incomplete_paths.pop
-        @caves[path[:last]].each do |to|
-          if to == 'end'
-            complete_paths += 1
-          elsif to == to.upcase || ! path[to]
+          elsif allow_dupe && path[:dupe].nil?
             new_path = path.clone
             new_path[:last] = to
-            new_path[to] = true
-            incomplete_paths << new_path
-          elsif path[:dupe].nil?
-            new_path = path.clone
-            new_path[:last] = to
-            new_path[to] = true
             new_path[:dupe] = to
             incomplete_paths << new_path
           end
@@ -59,5 +41,39 @@ module Aoc2021
       return complete_paths
     end
 
+    # ~0.35s for all tests
+    # depth-first recursive; one re-used data-structure
+    def cave_search_df(path, allow_dupe = false)
+      complete_paths = 0
+      @caves[path[:last]].each do |to|
+        if to == :end
+          complete_paths += 1
+        elsif @upcase_lookup[to] || path[to].nil?
+          old_last = path[:last]
+          path[:last] = to
+          path[to] = true
+          complete_paths += cave_search_df(path, allow_dupe)
+          path[:last] = old_last
+          path[to] = nil
+        elsif allow_dupe && path[:dupe].nil?
+          old_last = path[:last]
+          path[:last] = to
+          path[:dupe] = to
+          complete_paths += cave_search_df(path, allow_dupe)
+          path[:last] = old_last
+          path[:dupe] = nil
+        end
+      end
+      return complete_paths
+    end
+
+    def part1!
+      cave_search_df({ :last => :start })
+    end
+
+    def part2!
+      cave_search_df({ :last => :start }, true)
+    end
   end
+
 end
