db943a8e4ae04e9c0913e8fc204395776c42a147
caneylan <caneylan>
2021-12-16 01:49:24 -0500

    2021 day 16

--- lib/aoc2021/day16.rb
+++ lib/aoc2021/day16.rb
@@ -1,28 +1,9 @@
 module Aoc2021
   class Day16 < Solution
 
-    @@hex_to_bin = {
-      "0" => "0000",
-      "1" => "0001",
-      "2" => "0010",
-      "3" => "0011",
-      "4" => "0100",
-      "5" => "0101",
-      "6" => "0110",
-      "7" => "0111",
-      "8" => "1000",
-      "9" => "1001",
-      "A" => "1010",
-      "B" => "1011",
-      "C" => "1100",
-      "D" => "1101",
-      "E" => "1110",
-      "F" => "1111",
-    }
-
     def initialize(filename)
       super(filename)
-      @stream = input.first.chars.map { |c| @@hex_to_bin[c] }.join('').chars
+      @stream = input.first.chars.map { |c| "%04d" % c.to_i(16).to_s(2) }.join('').chars
     end
 
     def parse_packet!
