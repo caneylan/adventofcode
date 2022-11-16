131d56c7d45a506045eff849fa01471f940de9aa
caneylan <caneylan>
2022-11-16 09:53:51 -0500

    2021 day 22 -- cleanup

--- lib/aoc2021/day22.rb
+++ lib/aoc2021/day22.rb
@@ -28,29 +28,30 @@ module Aoc2021
                        )
                      ]
                    end.select { |c| c[1].size > 0 }
+
       lil_cubes = [ cube_steps.shift[1] ]  # assuming the first cuboid is an "on"
       until cube_steps.empty?
         cube_step = cube_steps.shift
         action = cube_step[0]
         cube = cube_step[1]
         if action                             # action:true means we're turning coords on
-          overlaps = false                    # we only add this cube to lil_cubes if it
-          lil_cubes.each do |lil_cube|        # doesn't intersect with any existing lil_cube
-            diff = cube - lil_cube            # if a lil_cube overlaps this cube, then we
-            if diff                           # enqueue the parts of cube that don't overlap
-              diff.each { |c| cube_steps.unshift([action, c]) } # for future processing
+          overlaps = false                    # we only add this cube to lil_cubes if it doesn't intersect
+          lil_cubes.each do |lil_cube|        #   with any existing lil_cube
+            diff = cube - lil_cube            # if a lil_cube overlaps this cube, then we enqueue the
+            if diff                           #   parts of cube that don't overlap for future processing
+              diff.each { |c| cube_steps.unshift([action, c]) }
               overlaps = true
               break
             end
           end
           lil_cubes << cube unless overlaps   # if we get here without an overlap, then it's a lil_cube
         else
-          lil_cubes_new = []                  # action:false means we're turning existing
-          lil_cubes.each do |lil_cube|        # coords off.  so walk through the lil_cubes
-            diff = lil_cube - cube            # and discard any parts of a lil_cube that
-            if diff                           # overlap with this "off" cube
-              lil_cubes_new += diff           # if there's no overlap between a lil_cube and this
-            else                              # "off" cube, then we just keep it
+          lil_cubes_new = []                  # action:false means we're turning existing coords off
+          lil_cubes.each do |lil_cube|        # so walk through the lil_cubes and discard any parts
+            diff = lil_cube - cube            #   of a lil_cube that overlap with this "off" cube
+            if diff                           # if there's no overlap between a lil_cube and this
+              lil_cubes_new += diff           #   "off" cube, then we just keep it
+            else
               lil_cubes_new << lil_cube
             end
           end
@@ -84,71 +85,43 @@ module Aoc2021
       end
 
       # if self and other intersect, then return the cubes of self
-      # with other taken away
+      # that remain with other taken away
       def -(other)
-        return nil unless z.min <= other.z.max && z.max >= other.z.min &&
-                          y.min <= other.y.max && y.max >= other.y.min && 
-                          x.min <= other.x.max && x.max >= other.x.min
+        return nil unless x.min <= other.x.max && x.max >= other.x.min &&
+                          y.min <= other.y.max && y.max >= other.y.min &&
+                          z.min <= other.z.max && z.max >= other.z.min
         new_cubes = []
 
-        # starting dimension doesn't matter.
-        # first, chop off the bits of self that are outside of other in the z-axis
-        if z.min >= other.z.min && z.max > other.z.max
+        # starting dimension doesn't matter
+        # we only need the intersection ranges for the first two dimensions we cut along
+        z_intersect = [z.min, other.z.min].max..[z.max, other.z.max].min
+        y_intersect = [y.min, other.y.min].max..[y.max, other.y.max].min
+       
+        # chop off the bit of self that's above and/or below other in the z-axis
+        if z.max > other.z.max
           new_cubes << Cuboid.new(x, y, (other.z.max + 1)..z.max)
-          remaining_z = z.min..other.z.max
-        elsif z.min >= other.z.min && z.max <= other.z.max
-          remaining_z = z.min..z.max
-        elsif z.min < other.z.min && z.max <= other.z.max
+        end
+        if z.min < other.z.min
           new_cubes << Cuboid.new(x, y, z.min..(other.z.min - 1))
-          remaining_z = other.z.min..z.max
-        else
-          new_cubes << Cuboid.new(x, y, (other.z.max + 1)..z.max)
-          new_cubes << Cuboid.new(x, y, z.min..(other.z.min - 1))
-          remaining_z = other.z.min..other.z.max
         end
 
-        # from here, what's left of the z-axis completely overlaps with the body of other
-        # next, chop off the bits of self that are outside of other in the y-axis
-        if y.min >= other.y.min && y.max > other.y.max
-          new_cubes << Cuboid.new(x, (other.y.max + 1)..y.max, remaining_z)
-          remaining_y = y.min..other.y.max
-        elsif y.min >= other.y.min && y.max <= other.y.max
-          remaining_y = y.min..y.max
-        elsif y.min < other.y.min && y.max <= other.y.max
-          new_cubes << Cuboid.new(x, y.min..(other.y.min - 1), remaining_z)
-          remaining_y = other.y.min..y.max
-        else
-          new_cubes << Cuboid.new(x, (other.y.max + 1)..y.max, remaining_z)
-          new_cubes << Cuboid.new(x, y.min..(other.y.min - 1), remaining_z)
-          remaining_y = other.y.min..other.y.max
+        # chop off the remaining bit of self that's above and/or below other in the y-axis
+        if y.max > other.y.max
+          new_cubes << Cuboid.new(x, (other.y.max + 1)..y.max, z_intersect)
+        end
+        if y.min < other.y.min
+          new_cubes << Cuboid.new(x, y.min..(other.y.min - 1), z_intersect)
         end
 
-        # finally, cut off the remaining bits of self that are out in the z-axis
-        if x.min >= other.x.min && x.max > other.x.max
-          new_cubes << Cuboid.new((other.x.max + 1)..x.max, remaining_y, remaining_z)
-        elsif x.min >= other.x.min && x.max <= other.x.max
-          # don't care
-        elsif x.min < other.x.min && x.max <= other.x.max
-          new_cubes << Cuboid.new(x.min..(other.x.min - 1), remaining_y, remaining_z)
-        else
-          new_cubes << Cuboid.new((other.x.max + 1)..x.max, remaining_y, remaining_z)
-          new_cubes << Cuboid.new(x.min..(other.x.min - 1), remaining_y, remaining_z)
+        # chop off what's left of self that's above and/or below other in the x-axis
+        if x.max > other.x.max
+          new_cubes << Cuboid.new((other.x.max + 1)..x.max, y_intersect, z_intersect)
+        end
+        if x.min < other.x.min
+          new_cubes << Cuboid.new(x.min..(other.x.min - 1), y_intersect, z_intersect)
         end
 
         return new_cubes
-      end
-
-      def &(other)
-        x_int = intersection(x, other.x) or return nil
-        y_int = intersection(y, other.y) or return nil
-        z_int = intersection(z, other.z) or return nil
-        Cuboid.new(x_int, y_int, z_int)
-      end
-
-      def intersection(a, b)
-        if a.min <= b.max && a.max >= b.min
-          [a.min, other.min].max..[a.max, b.max].min
-        end
       end
 
       def to_s
