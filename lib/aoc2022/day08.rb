278a160528d688a605a7156d1ee96bb50aec8d43
caneylan <caneylan>
2022-12-08 15:45:28 -0500

    2022 day 8
    re-implementation based on a solution here:
    https://old.reddit.com/r/adventofcode/comments/zfpnka/2022_day_8_solutions/izd90g1/
    
    because i thought it was neat.
    
    note: my loop body re-implements the original, which was creating new arrays
    called `lower`, which isn't necessary, since the only information used is the
    index of the first higher tree.

--- lib/aoc2022/day08.rb
+++ lib/aoc2022/day08.rb
@@ -3,38 +3,26 @@ module Aoc2022
   class Day08 < Solution
 
     def parse_input
-      @trees = @input.map(&:chars)
-      @row_max = @trees.length - 1
-      @col_max = @trees.first.length - 1
+      @grid = @input.map(&:chars)
+      @part1 = Array.new(@grid.length) { |i| Array.new(@grid.first.length, false) }
+      @part2 = Array.new(@grid.length) { |i| Array.new(@grid.first.length, 1) }
+
+      4.times do |iter|
+        @grid.each_index.to_a.product(@grid.first.each_index.to_a) do |(x, y)|
+          higher_i = (y+1).upto(@grid[x].length - 1).find_index { |_y| @grid[x][_y] >= @grid[x][y] }
+          @part1[x][y] ||= ! higher_i
+          @part2[x][y] *= (higher_i || @grid[x].length - y - 2) + 1
+        end
+        @grid, @part1, @part2 = [@grid, @part1, @part2].map { |a| a.transpose.map(&:reverse) }
+      end
     end
 
     def part1!
-      total = @trees.length * 2 + @trees.first.length * 2 - 4
-      1.upto(@row_max - 1) do |row_i|
-        1.upto(@col_max - 1) do |col_i|
-          visible   = ! (col_i - 1).downto(0).find { |i| @trees[row_i][i] >= @trees[row_i][col_i] }
-          visible ||= ! (row_i - 1).downto(0).find { |i| @trees[i][col_i] >= @trees[row_i][col_i] }
-          visible ||= ! (col_i + 1).upto(@col_max).find { |i| @trees[row_i][i] >= @trees[row_i][col_i] }
-          visible ||= ! (row_i + 1).upto(@row_max).find { |i| @trees[i][col_i] >= @trees[row_i][col_i] }
-          total += 1 if visible
-        end
-      end
-      total
+      @part1.map { |row| row.count(true) }.sum
     end
 
     def part2!
-      best = 0
-      1.upto(@row_max - 1) do |row_i|
-        1.upto(@col_max - 1) do |col_i|
-          left = col_i - ((col_i - 1).downto(0).find { |i| @trees[row_i][i] >= @trees[row_i][col_i] } || 0)
-          up   = row_i - ((row_i - 1).downto(0).find { |i| @trees[i][col_i] >= @trees[row_i][col_i] } || 0)
-          right = ((col_i + 1).upto(@col_max).find { |i| @trees[row_i][i] >= @trees[row_i][col_i] } || @col_max) - col_i
-          down  = ((row_i + 1).upto(@row_max).find { |i| @trees[i][col_i] >= @trees[row_i][col_i] } || @row_max) - row_i
-          score = left * right * up * down
-          best = score if score > best
-        end
-      end
-      best
+      @part2.map { |row| row.max }.max
     end
 
   end
