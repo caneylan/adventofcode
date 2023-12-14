6e5444df0641d1a27fc0b781de38fcabc010934f
caneylan <caneylan>
2023-12-13 20:26:28 -0500

    2023 day 12

--- spec/2023/aoc2023-day12_spec.rb
+++ spec/2023/aoc2023-day12_spec.rb
@@ -8,16 +8,6 @@ RSpec.describe Aoc2023::Day12 do
     expect(day.part1!).to eq(21)
   end
 
-  it "isn't this bad answer 1" do
-    day = Aoc2023::Day12.new ::File.expand_path("../../input/2023/day12/input1", __dir__)
-    expect(day.part1!).to be < 7713
-  end
-
-  it "isn't this bad answer 2" do
-    day = Aoc2023::Day12.new ::File.expand_path("../../input/2023/day12/input1", __dir__)
-    expect(day.part1!).not_to eq(6915)
-  end
-
   it "solves part 1" do
     day = Aoc2023::Day12.new ::File.expand_path("../../input/2023/day12/input1", __dir__)
     expect(day.part1!).to eq(7379)
