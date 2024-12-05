69c6dac0403df890fdc363c37724f6900f7a33f6
caneylan <caneylan>
2024-12-05 00:25:53 -0500

    2024 day 05

--- lib/aoc2024/day05.rb
+++ lib/aoc2024/day05.rb
@@ -3,14 +3,38 @@ module Aoc2024
   class Day05 < Solution
 
     def parse_input
+      @rules = Hash.new { |h, k| h[k] = {} }
+      @updates = []
+      input.each do |line|
+        case line
+        when /(\d+)\|(\d+)/
+          @rules[$1][$2] = true
+        when /\d+,/
+          @updates << line.split(',')
+        end
+      end
     end
 
     def part1!
-      return false
+      sum = 0
+      @updates.each do |update|
+        u = update.sort { |a, b| @rules[a][b] ? -1 : 1 }
+        if update == u
+          sum += u[u.size / 2].to_i
+        end
+      end
+      sum
     end
 
     def part2!
-      return false
+      sum = 0
+      @updates.each do |update|
+        u = update.sort { |a, b| @rules[a][b] ? -1 : 1 }
+        if update != u
+          sum += u[u.size / 2].to_i
+        end
+      end
+      sum
     end
 
   end
